7452ac0d5a4774ea06600d973b577a6fb274999d
Alex Labonne <alex.labonne@ekino.com>
2018-12-03 15:10:37 +0100

    Chore: rename to auditmyci

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-auditci
+auditmyci
 ======================
 
     A command line tool to audit your dependencies during your CI.
@@ -11,12 +11,12 @@ Depending on your package manager:
 
 - with NPM:
 ```
-npm install auditci
+npm install auditmyci
 ```
 
 - with Yarn:
 ```
-yarn add auditci
+yarn add auditmyci
 ```
 
 
@@ -26,10 +26,25 @@ Now that you installed it, you can use it:
 
 - with NPM:
 ```
-npm auditci
+npm auditmyci
 ```
 
 - with Yarn:
 ```
-yarn auditci
+yarn auditmyci
 ```
+
+#### Options:
+Options | What it does | Default
+|--|--|--|
+| ```-l, --low``` | Exit if there are low or higher vulnerabilities | false
+| ```-m, --moderate``` | Exit if there are moderate or higher vulnerabilities | false
+| ```-hi, --high``` | Exit if there are high or critical vulnerabilities | false
+| ```-c, --critical``` | Exit if there are critical vulnerabilities | true
+| ```-r, --report``` | Show the audit report | false
+| ```--help``` | Show help | false
+### License
+
+See [License](https://github.com/brainlulz/auditmyci/blob/master/LICENSE)
+
+Inspired by [npm-audit-ci](https://github.com/brainlulz/npm-audit-ci) from Revath S Kumar
--- index.js
+++ index.js
@@ -1,118 +1,113 @@
+#!/usr/bin/env node
+
 'use strict';
+
+const exec = require('child_process').exec;
+const process = require('process');
 const fs = require('fs');
 
-const packageManager = fs.existsSync('./yarn.lock') ? 'yarn': 'npm';
+const yarnFiles = fs.existsSync('./yarn.lock');
+const npmFiles =
+  fs.existsSync('./npm-shrinkwrap.json') ||
+  fs.existsSync('./package-lock.json');
+const packageManager = npmFiles ? 'npm' : 'yarn';
 
-const options = require('yargs')
-  .options({
-    'r': {
-      alias: 'report',
-      default: false,
-      describe: 'Show the audit report',
-      type: 'boolean'
-    }
+const program = require('commander')
+  .option('-l, --low [boolean]', 'Exit if there are low or higher vulnerabilities')
+  .option('-m, --moderate [boolean]', 'Exit if there are low or higher vulnerabilities')
+  .option('-hi, --high [boolean]', 'Exit if there are high or critical vulnerabilities')
+  .option('-c, --critical [boolean]', 'Exit if there are critical vulnerabilities', {
+    isDefault: true
   })
-  .help('help')
-  .argv;
+  .option('-r, --report [boolean]', 'Show the audit report')
+  .parse(process.argv)
 
-const argv = require('yargs')
-    .options({
-      'l': {
-        alias: 'low',
-        default: false,
-        describe: 'Exit even for low vulnerabilities',
-        type: 'boolean'
-      },
-      'm': {
-        alias: 'moderate',
-        default: false,
-        describe: 'Exit only when moderate or above vulnerabilities',
-        type: 'boolean'
-      },
-      'h': {
-        alias: 'high',
-        default: false,
-        describe: 'Exit only when high or above vulnerabilities',
-        type: 'boolean'
-      },
-      'c': {
-        alias: 'critical',
-        default: true,
-        describe: 'Exit only for critical vulnerabilities',
-        type: 'boolean'
-      },
-      'r': {
-        alias: 'report',
-        default: false,
-        describe: 'Show audit report',
-        type: 'boolean'
-      }
-    })
-    .help('help')
-    .argv;
-
-const parseMessage = (severityline, argv = {}) => {
-  if (severityline.indexOf('Severity:') === -1) {
-    return ''; 
-  }
-  const matches = severityline.match(/^(\D+|)((\d+)\D+[lL]ow|)(\D+|)((\d+)\D+[mM]oderate|)(\D+|)((\d+)\D+[hH]igh|)(\D+|)((\d+)\D+[cC]ritical|)/);
-  const lowCount = parseInt(matches[3]);
-  const moderateCount = parseInt(matches[6]);
-  const highCount = parseInt(matches[9]);
-  const criticalCount = parseInt(matches[12]);
-
-  if (argv.critical && criticalCount > 0) {
-    return 'CRITICAL';
+const parseMessage = (output, options = {}) => {
+  if (options.critical && output.critical > 0) {
+    return 'critical';
   }
 
-  if (argv.high && (criticalCount > 0 || highCount > 0)) {
-    return 'HIGH';
+  if (options.high && (output.critical > 0 || output.high > 0)) {
+    return 'high';
   }
 
-  if (argv.moderate && (criticalCount > 0 || highCount > 0 || moderateCount > 0)) {
-    return 'MODERATE';
+  if (
+    options.moderate &&
+    (output.critical > 0 || output.high > 0 || output.moderate > 0)
+  ) {
+    return 'moderate';
   }
 
-  if (argv.low && (criticalCount > 0 || highCount > 0 || moderateCount > 0 || lowCount > 0)) {
-    return 'LOW';
+  if (
+    options.low &&
+    (output.critical > 0 ||
+      output.high > 0 ||
+      output.moderate > 0 ||
+      output.low > 0)
+  ) {
+    return 'low';
   }
-
   return '';
-}
+};
 
-const run = () =>{
-  exec(`${packageManager} audit`, function (error, stdout, stderr) {
+const parseOutput = output => {
+  if (packageManager === 'yarn') {
+    const stdoutJSON = output.split('\n');
+    return JSON.parse(stdoutJSON[stdoutJSON.length - 2]).data.vulnerabilities;
+  }
+  return JSON.parse(output).metadata.vulnerabilities;
+};
+
+const run = () => {
+  if (npmFiles && yarnFiles) {
+    console.log(
+      'You have a package-lock.json or a npm-shrinkwrap.json file, and a yarn.lock file. Please use only one package manager.'
+    );
+    process.exit(1);
+  }
+  if (!npmFiles && !yarnFiles) {
+    console.log(
+      'You have no lock file. Either you are not using a package manager or your package manager is not supported yet.'
+    );
+  }
+  exec(`${packageManager} audit --json`, function (error, stdout, stderr) {
     if (stdout) {
-      if (stdout.indexOf('[+] no known vulnerabilities found') >= 0) {
-        return console.log('No issues :: SUCCESS');
+      const vulnerabilities = parseOutput(stdout);
+      const totalVulnerabilities =
+        vulnerabilities.critical +
+        vulnerabilities.high +
+        vulnerabilities.moderate +
+        vulnerabilities.low;
+
+      if (totalVulnerabilities === 0) {
+        if (program.report) {
+          console.log(stdout);
+        }
+        return console.log('\x1b[32m', 'SUCCESS: No vulnerability found');
       }
-      
-      if (argv.report) {
+
+      if (program.report) {
         console.log(stdout);
       }
 
-      const logArr = stdout.split('\n').filter(line => line);
-      const severityline = logArr[logArr.length - 1];
-      const severityType = parseMessage(severityline, argv);
+      const severityType = parseMessage(vulnerabilities, program);
       switch (severityType) {
-        case 'CRITICAL':
-        case 'HIGH':
-        case 'MODERATE':
-        case 'LOW':
-          let message = `FAILURE :: ${severityType}`;
+        case 'critical':
+        case 'high':
+        case 'moderate':
+        case 'low':
+          const message = `FAILURE: ${totalVulnerabilities} total vulnerability(ies)`;
 
-          if (!argv.report) {
-            message = `${message} :: ${severityline}`;
-          }
-
-          console.log(message);
+          console.log('\x1b[31m', message);
           process.exit(1);
           return;
-        case '':      
+        case '':
         default:
-          if (!argv.report) {
-            console.log(severityline);
+          if (program.report) {
+            console.log(stdout);
           }
+
+          console.log("\x1b[32m", 'SUCCESS: No vulnerability found');
 
           return;
       }
@@ -121,9 +116,6 @@ const run = () =>{
       console.log('exec error: ' + error);
     }
   });
-}
-
-module.exports = {
-  run: run,
-  parseMessage: parseMessage
 };
+
+module.exports = run();
